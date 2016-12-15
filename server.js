e98639203805ccf19fcafd5b9ced21ee776b3663
Megan Walter <megwalter@hotmail.com>
2016-12-15 18:00:27 -0500

    added body parser and new post route handler

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Meet mom for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go to the store',
-  completed: false
-}, {
-  id: 3,
-  description: 'Go to the gym',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -38,12 +31,19 @@ app.get('/todos/:id', function (req, res) {
   } else {
     res.status(404).send();
   }
-  // for (var i = 0; i < todos.length; i++) {
-  //   if (todos[i].id === todoId) {
-  //     res.json(todos[i]);
-  //     return;
-  //   }
-  // }
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+  var body = req.body;
+
+  body.id = todoNextId;
+  todos.push(body);
+  todoNextId++;
+  console.log(todos);
+  //push body to todos
+  //add id field and increment
+  res.json(body);
 });
 
 app.listen(PORT, function () {
